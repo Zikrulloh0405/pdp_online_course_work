456e74fcea7b8093c3c08b0690e59575fcac1113
Zikrulloh0405 <zikrillohakramov2@gmail.com>
2024-12-01 12:32:31 +0500

    first commit

---
+++ bin/pdp_online_course_work.dart
@@ -0,0 +1,5 @@
+import 'package:pdp_online_course_work/pdp_online_course_work.dart' as pdp_online_course_work;
+
+void main(List<String> arguments) {
+  print('Hello world: ${pdp_online_course_work.calculate()}!');
+}
---
+++ test/pdp_online_course_work_test.dart
@@ -0,0 +1,8 @@
+import 'package:pdp_online_course_work/pdp_online_course_work.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
